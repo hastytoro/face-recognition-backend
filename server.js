ace25750f18091be0f16f134afe0c7767eeb2192
Luigi Lupini <luigi.lupini@gmail.com>
2021-01-20 22:10:35 +0200

    added knex services with a local postgres db for user access control

--- server.js
+++ server.js
@@ -1,81 +1,112 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
+const knex = require('knex');
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'password',
+    database: 'facedb',
+  },
+});
+
+const postmanQuery = {
+  // use the following examples:
   users: [
     {
-      id: '007',
       name: 'bond',
       email: 'jamesbond@mi6.gov',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
+      password: 'skyFall',
     },
     {
-      id: '123',
       name: 'mallory',
       email: 'garethmallory@mi6.gov',
-      password: 'chairman',
-      entries: 0,
-      joined: new Date(),
+      password: 'skyFall',
     },
   ],
 };
 
-app.get('/', (req, res) => res.send(database.users));
+app.get('/', (req, res) => {
+  db.select('*')
+    .from('users')
+    .then(data => res.json(data));
+});
 
 app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json('success');
-  } else {
-    res.status(400).json('denied');
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json('unable to signin user.'));
+      } else {
+        res.status(400).json('wrong credentials.');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials.'));
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, (err, hash) => console.log(hash));
-  database.users.push({
-    id: '123',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then(user => res.json(user[0]));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('unable to register user.'));
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let isFound = false;
-  database.users.map(user => {
-    if (user.id === id) {
-      isFound = true;
-      res.json(user);
-    }
-  });
-  if (!isFound) res.status(400).json('user not found');
+  db.select('*')
+    .from('users')
+    .where({
+      id: id,
+    })
+    .then(user => {
+      if (user.length) res.json(user[0]);
+      else res.status(400).json('user not found');
+    })
+    .catch(err => res.status(400).json('error getting user profile.'));
 });
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let isFound = false;
-  database.users.map(user => {
-    if (user.id === id) {
-      isFound = true;
-      user.entries++;
-      res.json(user.entries);
-    }
-  });
-  if (!isFound) res.status(400).json('user not found');
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('error getting user entries.'));
 });
 
 const PORT = 3000;
